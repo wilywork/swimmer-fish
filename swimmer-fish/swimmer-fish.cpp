40f16c94e78b3e8d90c42d98718fded394dba5b2
Vsmee <viniciusmoreira@edu.univali.br>
2020-10-27 16:11:20 -0300

    Ta indo
    Eu tava testando umas coisas aqui e ficou assim

--- swimmer-fish/swimmer-fish.cpp
+++ swimmer-fish/swimmer-fish.cpp
@@ -1,28 +1,124 @@
-#define SDL_MAIN_HANDLED
-
+// https://www.youtube.com/watch?v=IDa8hfl_9Vk&t=217s&ab_channel=elieserdejesus
 #include <iostream>
 #include <string.h>
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
-
 using namespace std;
 
-const int LARGURA = 900;
-const int ALTURA = 600;
+SDL_Texture* CarregaTextura(const char* imagem, SDL_Renderer* renderizador)
+{
+    SDL_Surface* surface = SDL_LoadBMP(imagem); // Pega uma imagem/arquivo para a surface
+    if (!surface) {
+        cout << "Erro ao carregar: " << imagem << endl;
+    }
 
-int main() {
+    SDL_Texture* textura = SDL_CreateTextureFromSurface(renderizador, surface); // Textura recebe a surface
 
-    SDL_Window* janela = NULL;
+    SDL_FreeSurface(surface); // Limpa a surface
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    return textura;
+};
 
-    // inicia a JANELA
-    janela = SDL_CreateWindow("Janela", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 
-    SDL_Delay(5000);
+int main()
+{
+
+    SDL_Window* janela;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) // Inicia a SDL
+        cout << SDL_GetError() << endl;
+
+    janela = SDL_CreateWindow("Janela", // inicia a JANELA
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        0);
+
+    SDL_Renderer* renderizador = SDL_CreateRenderer(janela, // Renderiza coisas na janela
+        -1,
+        SDL_RENDERER_ACCELERATED);
+
+    // Imagens renderizadas
+
+    SDL_Texture* fundo = CarregaTextura("imagens/fundoJogo.bmp", renderizador);
+    SDL_Texture* peixe = CarregaTextura("imagens/peixe.bmp", renderizador);
+    SDL_Texture* coral = CarregaTextura("imagens/coral.bmp", renderizador);
+
+    bool gameOver = false; // Variavel para manter o jogo aberto
+
+    int peixeMov = 0;
+    int coralMov = 0;
+    while (!gameOver) {
+
+        SDL_Event evento;
+        while (SDL_PollEvent(&evento)) {
+            switch (evento.type) {
+            case SDL_QUIT:
+                gameOver = true;
+                break;
+
+            case SDL_KEYDOWN:
+                switch (evento.key.keysym.sym) {
+                case SDLK_UP:
+                case SDLK_w:
+                    cout << "CIMA" << endl;
+                    peixeMov -= 30;
+                    break;
+                }
+                break;
+            }
+        }
+
+        SDL_RenderClear(renderizador); // Limpa a janela
+
+
+        SDL_RenderCopy(renderizador, fundo, NULL, NULL);
+
+        SDL_Rect destinoPeixe;
+        destinoPeixe.w = 100;
+        destinoPeixe.h = 100;
+        destinoPeixe.x = 50;
+        destinoPeixe.y = peixeMov * 3;
+        if (destinoPeixe.y >= 520) {
+            destinoPeixe.y = 520;
+        }
+        else
+        {
+            peixeMov++;
+        }
+        if (destinoPeixe.y < 0)
+        {
+            peixeMov = 0;
+        }
+        SDL_RenderCopy(renderizador, peixe, NULL, &destinoPeixe);
+
+
+
+        SDL_Rect destinoCoral;
+        destinoCoral.w = 40;
+        destinoCoral.h = 60 * (404 / 122);
+        destinoCoral.x = 700 - (coralMov * 5);
+        destinoCoral.y = 600 - destinoCoral.h;
+        SDL_RenderCopy(renderizador, coral, NULL, &destinoCoral);
+        coralMov++;
+        if (destinoCoral.x <= -100) {
+            coralMov = 0;
+        }
+
+        if (destinoPeixe.y + destinoPeixe.h > destinoCoral.y) {
+            if (destinoPeixe.x + destinoPeixe.w > destinoCoral.x) {
+                cout << "TESTE" << endl;
+                peixeMov = 0;
+            }
+        }
+
+        SDL_RenderPresent(renderizador); // Cola coisas na janela
+        SDL_Delay(1000 / 60); // 60 fps
+
+    }
+
     SDL_DestroyWindow(janela);
     SDL_Quit();
-
-    // bruno, blz
-
-	return 0;
+    return 0;
 }
