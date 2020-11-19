a1e6f0d5794fa2586d271d1d08ff70c8d19d7bf8
WilyWork <xandy3_sjb@hotmail.com>
2020-11-18 21:18:17 -0300

    fix bug mouse;

--- swimmer-fish/Eventos.cpp
+++ swimmer-fish/Eventos.cpp
@@ -2,25 +2,31 @@
 
 void FuncEventos(bool& gameOver, int& peixeMov, int& pressUpPeixe) {
     SDL_Event evento;
-    while (SDL_PollEvent(&evento)) {
+    while (SDL_PollEvent(&evento) != 0) {
         switch (evento.type) {
         case SDL_QUIT:
             gameOver = true;
             break;
-
         case SDL_KEYDOWN:
+            switch (evento.key.keysym.sym) {
+                case SDLK_UP:
+                case SDLK_w:
+                    if (peixeMov >= -10) {
+                        pressUpPeixe = true;
+                        peixeMov -= 1;
+                    }
+                    break;
+                }
+            break;
+        case SDL_KEYUP:
             switch (evento.key.keysym.sym) {
             case SDLK_UP:
             case SDLK_w:
-                if (peixeMov >= -10) {
-                    pressUpPeixe = true;
-                    peixeMov -= 1;
-                }
+                    pressUpPeixe = false;
                 break;
             }
             break;
         default:
-            pressUpPeixe = false;
             break;
         }
     }
