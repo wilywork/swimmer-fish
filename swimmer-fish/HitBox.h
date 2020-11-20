adb64fc36e2c3c63afd1dc5bc04a4e1906c22f52
WilyWork <xandy3_sjb@hotmail.com>
2020-11-19 21:24:24 -0300

    ajustes;

--- swimmer-fish/HitBox.h
+++ swimmer-fish/HitBox.h
@@ -5,13 +5,13 @@
 
 int RandTamCoral();
 
-void HitBoxCoral(SDL_Rect destinoCoral, SDL_Rect destinoPeixe, int& peixeMov, int& score, int& TAMpeixe, int& comidaRandX, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
+void HitBoxCoral(SDL_Rect destinoCoral, SDL_Rect &destinoPeixe, int& peixeMov, int& score, int& comidaRandX, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
 
-void HitBoxComida(SDL_Rect destinoComida, SDL_Rect destinoPeixe, int& TAMpeixe, int& comidaMov, int& score, int& comidaRandY, int& comidaMOV);
+void HitBoxComida(SDL_Rect destinoComida, SDL_Rect &destinoPeixe, int& comidaMov, int& score, int& comidaRandY, int& comidaMOV);
 
-void HitBoxLinha(SDL_Rect destinoLinha, SDL_Rect destinoPeixe, int& peixeMov, int& score, int& TAMpeixe, int& comidaRandX, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
+void HitBoxLinha(SDL_Rect destinoLinha, SDL_Rect &destinoPeixe, int& peixeMov, int& score, int& comidaRandX, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
 
-void HitBoxAnzol(SDL_Rect destinoAnzol, SDL_Rect destinoPeixe, int& peixeMov, int& score, int& TAMpeixe, int& comidaRandY, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
+void HitBoxAnzol(SDL_Rect destinoAnzol, SDL_Rect &destinoPeixe, int& peixeMov, int& score, int& comidaRandY, int& coralMov, int& linhaMOV, int& comidaMov, int& TAMcoral);
 
 
 #endif // JOGO_H
--- swimmer-fish/Peixe.h
+++ swimmer-fish/Peixe.h
@@ -3,14 +3,13 @@
 #define PEIXE_H
 
 typedef struct {
-	int tamanho;
 	int altura;
 	const char* textura;
 	SDL_Rect estrutura;
 	SDL_Texture* texturaCarregada;
 } Peixe;
 
-Peixe SpawnPeixe(SDL_Renderer* renderizador, const char* textura, int tamanho, int altura);
+Peixe SpawnPeixe(SDL_Renderer* renderizador, const char* textura, int altura);
 
 void UpdatePeixe(SDL_Renderer* renderizador, Peixe peixe);
 
